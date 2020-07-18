730a677ae500abc1ed2ff9cf9d8719eee5c1465b
大魔王 <1812613402@qq.com>
2020-07-18 15:29:42 +0800

    123

---
+++ LinXi_ERP/LinXi_IdentityServer/Controllers/AuthorizaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinXi_IdentityServer.Controllers
+{
+    public class AuthorizaController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ LinXi_ERP/LinXi_IdentityServer/Controllers/identityController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinXi_IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class identityController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return new JsonResult(
+                from c in User.Claims select new { c.Type, c.Value }
+                );
+        }
+
+        public IActionResult Check(string name, string pwd)
+        {
+            return new JsonResult(
+                from c in User.Claims select new { c.Type, c.Value }
+                );
+        }
+    }
+}
